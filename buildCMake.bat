cde1ede790290e89c84e6a271802909b2af39db3
Egor <egorbelousko@mail.ru>
2024-07-03 12:32:26 +0700

    no message

--- buildCMake.bat
+++ buildCMake.bat
@@ -1,8 +1,5 @@
 @echo off
 
-echo cmake_minimum_required(VERSION 3.10) > CMakeLists.txt
-echo project(Kits) >> CMakeLists.txt
-echo add_executable(KitsCMake Kits.cpp) >> CMakeLists.txt
 mkdir KitsCMake
 cd KitsCmake
 cmake ../
